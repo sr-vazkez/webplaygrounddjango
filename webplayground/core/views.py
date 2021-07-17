f002f45c955bc7ca7d77a8a2f2a85fe83a1deb59
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-17 02:24:02 -0500

    templateviews

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-def home(request):
-    return render(request, "core/home.html")
 
-def sample(request):
-    return render(request, "core/sample.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
