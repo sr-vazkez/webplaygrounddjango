c6dd0d403e322cdada8ee86757ebdd69a9fc7d02
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-18 14:34:31 -0500

    redireccionamiento

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.urls import reverse 
 from .models import Page
 
 # Create your views here.
@@ -13,3 +14,6 @@ class PageDetailView(DetailView):
 class PageCreate(CreateView):
     model = Page
     fields = ['title', 'content', 'order']
+    def get_success_url(self):
+        return reverse(pages:pages)
+    
