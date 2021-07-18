bed8999cda2973c66e3bddb7e86eacc8dee30503
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-18 13:55:58 -0500

    pagina de detalle

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-from .views import PageListView
+from .views import PageListView, PageDetailView
 urlpatterns = [
     path('', PageListView.as_view(), name='pages'),
-    path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
+    path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
 ]
