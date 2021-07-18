dff11ea95152484818c35844d73ade01eaef5853
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-18 13:49:58 -0500

    pasando a clases

--- webplayground/pages/urls.py
+++ webplayground/pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-
+from .views import PageListView
 urlpatterns = [
-    path('', views.pages, name='pages'),
+    path('', PageListView.as_view(), name='pages'),
     path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
 ]
--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,10 +1,10 @@
+from django.views.generic.list import ListView
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Page
 
 # Create your views here.
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
+class PageListView(ListView):
+    model = Page
 
 def page(request, page_id, page_slug):
     page = get_object_or_404(Page, id=page_id)
