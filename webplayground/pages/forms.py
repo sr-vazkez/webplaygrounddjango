706331721a6a4169e7325ecaff3ee0628169ac8e
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-20 00:24:14 -0500

    agregando widgets del form

--- webplayground/pages/forms.py
+++ webplayground/pages/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import widgets
 
 from .models import Page
 
@@ -7,3 +8,8 @@ class PageForm(forms.ModelForm):
      class Meta:
           model = Page
           fields = ['title','content','order']
+          widgets = {
+          'title': forms.TextInput(attrs={'class':'form-control'}),
+          'content': forms.TextArea(attrs={'class':'form-control'}),
+          'order': forms.TextNumber(attrs={'class':'form-control'}),
+          }
