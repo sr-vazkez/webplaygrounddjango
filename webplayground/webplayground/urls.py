3f60a754fc6d4103103bf780e3fb20b19f1d588a
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-20 21:39:15 -0500

    vistas y rutas del singup

--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #Ruta de auth
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls')),
+    
 ]
