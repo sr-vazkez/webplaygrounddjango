d7fe5017d892d44d68ed99f92dfa32e3b1d3d533
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-25 23:22:37 -0500

    Generando validacion email

---
+++ webplayground/registration/forms.py
@@ -0,0 +1,10 @@
+from django import forms 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserCreationFormWhitEmail(UserCreationForm):
+     email = forms.EmailField(required=True, help_text="Campo obligatorio, 254 caracteres como maximo que debe ser valido")
+
+     class Meta:
+          model = User
+          fields = {"username", "email", "password1", "password2"}
