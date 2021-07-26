23161a0faf5bea0bc111f56f891348588395deca
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-25 23:09:54 -0500

    sobreescribiendo en tiempo de execucion

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-
+from django import forms
 # Create your views here.
 
 
@@ -11,4 +11,12 @@ class SignUpView(CreateView):
 
   def get_success_url(self):
     return reverse_lazy('login') + '?register'
+
+  def get_form(self, form_class=None):
+    form = super(SingUpView, self).get_form()
+    #modificar en tiempo real
+    form.fields['username'].widget = forms.TextInput(attr={'class:'form-control mb-2', 'placeholder':'Nombre de Usuario'})
+    form.fields['password1'].widget = forms.PasswordInput(attr={'class:'form-control mb-2', 'placeholder':'Password '})
+    form.fields['password2'].widget = forms.PasswordInput(attr={'class:'form-control mb-2', 'placeholder':'Repite tu password'})
+    return form  
   
