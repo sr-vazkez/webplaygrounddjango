5c22e47045038a987d1f5735ad9bc0f3660071d4
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-25 22:57:23 -0500

    un login simple funciona;

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -7,6 +7,8 @@ from django.urls import reverse_lazy
 
 class SignUpView(CreateView):
   form_class = UserCreationForm
-  success_url = reverse_lazy('login')
   template_name = 'registration/signup.html'
+
+  def get_success_url(self):
+    return reverse_lazy('login') + '?register'
   
