1e551ddfac9335a92c49c14fcd42dc10d83105c7
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-28 00:40:08 -0500

    add new template y configurando modelo

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,23 +1,30 @@
 from .forms import UserCreationFormWithEmail
 from django.views.generic import CreateView
+from django.views.generic.base import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django import forms
 # Create your views here.
 
 
 class SignUpView(CreateView):
-  form_class = UserCreationFormWithEmail
-  template_name = 'registration/signup.html'
+    form_class = UserCreationFormWithEmail
+    template_name = 'registration/signup.html'
 
-  def get_success_url(self):
-    return reverse_lazy('login') + '?register'
+    def get_success_url(self):
+        return reverse_lazy('login') + '?register'
 
-  def get_form(self, form_class=None):
-    form = super(SingUpView, self).get_form()
-    #modificar en tiempo real
-    form.fields['username'].widget = forms.TextInput(attr={'class:'form-control mb-2', 'placeholder':'Nombre de Usuario'})
-    form.fields['email'].widget = forms.EmailInput(attr={'class:'form-control mb-2', 'placeholder':'Correo Electronico'})
-    form.fields['password1'].widget = forms.PasswordInput(attr={'class:'form-control mb-2', 'placeholder':'Password '})
-    form.fields['password2'].widget = forms.PasswordInput(attr={'class:'form-control mb-2', 'placeholder':'Repite tu password'})
-    return form  
-  
+    def get_form(self, form_class=None):
+        form = super(SingUpView, self).get_form()
+        # modificar en tiempo real
+        form.fields['username'].widget = forms.TextInput(attr={'class:'form-control mb-2', 'placeholder':'Nombre de Usuario'})
+        form.fields['email'].widget = forms.EmailInput(attr={'class:'form-control mb-2', 'placeholder':'Correo Electronico'})
+        form.fields['password1'].widget = forms.PasswordInput(attr={'class:'form-control mb-2', 'placeholder':'Password '})
+        form.fields['password2'].widget = forms.PasswordInput(attr={'class:'form-control mb-2', 'placeholder':'Repite tu password'})
+        return form
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileUpdate(TemplateView):
+    template_name = 'registration/profile_form.html'
