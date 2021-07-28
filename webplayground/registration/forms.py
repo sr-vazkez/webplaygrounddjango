62c59250cda0cb93e998828b24e2da6b0b56e324
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-28 01:37:08 -0500

    mejorando form desde el server

--- webplayground/registration/forms.py
+++ webplayground/registration/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserCreationFormWithEmail(UserCreationForm):
@@ -17,3 +18,14 @@ class UserCreationFormWithEmail(UserCreationForm):
             raise forms.ValidationError(
                 "El email ya fue registrado, usa otro :p")
         return email
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio', 'link']
+        widgets = {
+            'avatar': forms.ClearableFileInput(attrs={'class': 'form-control-file mt-3'}),
+            'bio': forms.Textarea(attrs={'class': 'form-control-file mt-3', 'rows': 3, 'placeholder': 'Biografia'}),
+            'link': forms.URLInput(attrs={'class': 'form-control-file mt-3', 'placeholder': 'Enlace'}),
+        }
