bff799f63886fd0da479c52a08bbc72607a4a0b2
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-28 02:27:59 -0500

    borrando fotos pasadas

--- webplayground/registration/models.py
+++ webplayground/registration/models.py
@@ -5,9 +5,16 @@ from django.db.models.signals import post_save
 # Create your models here.
 
 
+def custom_upload_to(instance, filename):
+    old_instance = Profile.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return 'profiles/' + filename
+
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='profiles', null=True, blank=True)
+         user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(
+        upload_to=custom_upload_to, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     link = models.URLField(max_length=200, null=True, blank=True)
 
