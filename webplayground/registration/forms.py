7f0490421405c66b44537f09feec7e0afb0e27f0
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-28 01:18:48 -0500

    corrigiendo errores y realizando migrations

--- webplayground/registration/forms.py
+++ webplayground/registration/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class UserCreationFormWhitEmail(UserCreationForm):
+class UserCreationFormWithEmail(UserCreationForm):
     email = forms.EmailField(
         required=True, help_text="Campo obligatorio, 254 caracteres como maximo que debe ser valido")
 
