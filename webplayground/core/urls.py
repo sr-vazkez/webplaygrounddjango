d60f521ea8afbcf09e1474297b6127abb28f0bcc
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-31 23:30:24 -0500

    arreglando rutas core

--- webplayground/core/urls.py
+++ webplayground/core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ProfileListView, ProfileDetailView
+from .views import HomePageView, SamplePageView
 
-urlpatterns = ([
-    path('', ProfileListView.as_view(), name='list'),
-    path('<username>/', ProfileDetailView.as_view(), name='detail'),
-], "profiles")
+urlpatterns = [
+    path('', HomePageView.as_view(), name="home"),
+    path('sample/', SamplePageView.as_view(), name="sample"),
+]
