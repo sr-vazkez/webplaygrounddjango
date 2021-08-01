eb292d27cecbc6377448dae5e02934e7ebe31110
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-07-31 22:16:26 -0500

    agregando app profile

--- webplayground/webplayground/settings.py
+++ webplayground/webplayground/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'core',
     'pages.apps.PagesConfig',
+    'profile',
 ]
 
 MIDDLEWARE = [
--- webplayground/webplayground/urls.py
+++ webplayground/webplayground/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from django.conf import settings
+from profiles.urls import profiles_patterns
+
 urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
@@ -24,7 +26,7 @@ urlpatterns = [
     # Ruta de auth
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
-
+    path('profiles/', include(profiles_patterns)),
 ]
 if settings.DEBUG:
     from django.conf.urls.static import static
